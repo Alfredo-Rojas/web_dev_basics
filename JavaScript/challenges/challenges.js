f2a7529d7a1600a98d3881b3910827ea3dbd5a60
Alfredo-Rojas <alfredot84@gmail.com>
2019-10-18 12:15:43 -0400

    Challenge 5 done

--- JavaScript/challenges/challenges.js
+++ JavaScript/challenges/challenges.js
@@ -98,3 +98,24 @@
 // }
 
 // findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
+// 5) Titlecase
+
+// function titleCase(str) {
+//   //Large
+//   // let words = str.toLowerCase().split(" ");
+//   // for (let i=0;i<words.length;i++) {
+//   //   words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//   // }
+//   // return words.join(" ");
+
+//   //Fancy
+//   let titled = str.toLowerCase().split(" ").map(function(elem) {
+//     return elem[0].toUpperCase() + elem.slice(1);
+//     console.log(elem);
+//   })
+//   return titled.join(" ");
+// }
+
+// titleCase("I'm a little tea pot");
