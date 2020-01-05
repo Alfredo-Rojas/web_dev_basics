2563bba8ccb34f6ca9c5bbdd0ac4d2fbdcbf5971
Alfredo-Rojas <alfredot84@gmail.com>
2020-01-05 07:27:55 -0500

    Sorting arrays with reduce

---
+++ WebApps/pong/main.js
@@ -0,0 +1,14 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+// ctx.beginPath();
+ctx.moveTo(0, 0,);
+ctx.lineTo(480, 322);
+ctx.strokeStyle = "red";
+ctx.stroke();
+
+// function draw() {
+//   requestAnimationFrame(draw);
+// }
+
+// requestAnimationFrame(draw);  
