5e071960ba6406eab31130f5692361b766699834
Alfredo-Rojas <alfredot84@gmail.com>
2020-01-06 12:55:23 -0500

    Chunky Monkey Challenge

--- JavaScript/challenges/challenges.js
+++ JavaScript/challenges/challenges.js
@@ -305,10 +305,25 @@
 
 //9) Truncate a String
 
-function truncateString(str, num) {
-  if (num >= str.length) return str;
-  if (num <= 3) return str.slice(0, num) + "...";
-   return (str.slice(0, num - 3)) + "...";
-} 
+// function truncateString(str, num) {
+//   if (num >= str.length) return str;
+//   if (num <= 3) return str.slice(0, num) + "...";
+//    return (str.slice(0, num - 3)) + "...";
+// } 
 
-truncateString("A-tisket a-tasket A green and yellow basket", 11);
+// truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+
+//10) Chunky Monkey
+
+function chunkyArrayInGroups(arr, size) {
+  let groups = [];
+
+  while (arr.length > 0) {
+    groups.push(arr.slice(0, size));
+    arr = arr
+  }
+  return groups;
+}
+
+chunkyArrayInGroups(["a", "b", "c", "d"], 2);
