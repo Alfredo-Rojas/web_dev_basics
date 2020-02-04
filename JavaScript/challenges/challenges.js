429e21f595e17b1514469c3eff178d9f532d5aad
Alfredo-Rojas <alfredot84@gmail.com>
2020-02-04 09:37:22 -0500

    Mutation challenge done!

--- JavaScript/challenges/challenges.js
+++ JavaScript/challenges/challenges.js
@@ -351,14 +351,18 @@
 
 //12) Mutation
 
-function mutation(arr) {
-  let firstWord = arr[0].toLowerCase();
-  let secondWord = arr[1].toLowerCase();
+// function mutation(arr) {
+//   let firstWord = arr[0].toLowerCase();
+//   let secondWord = arr[1].toLowerCase();
 
-  for (let i=0;i<secondWord.length;i++) {
-    if (firstWord.indexOf(secondWord[i]) === -1) return false;
-  }
-  return true;
-}
+//   for (let i=0;i<secondWord.length;i++) {
+//     if (firstWord.indexOf(secondWord[i]) === -1) return false;
+//   }
+//   return true;
+// }
 
-mutation(["Hello", "hey"]);
+// mutation(["Hello", "hey"]);
+
+
+
+
