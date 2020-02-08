3d7d89121e4257d0131227e4a08c62326536d263
Alfredo-Rojas <alfredot84@gmail.com>
2020-02-08 16:04:33 -0500

    Some Functions Practice added

---
+++ JavaScript/functionsPractice/main.js
@@ -0,0 +1,23 @@
+//ES5
+// function sayHello(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// let sayHello = function(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+//ES6
+// sayHello = (name) => console.log(`Hello ${name}`);
+
+// sayHello('Fred');
+
+
+//Scope 
+// var name = 'John';
+
+// (function() {
+//   //It's a good idea wrap the entire app!
+//   var name = 'Bla';
+//   console.log(name);
+// })();
