e097e6e690b638d3a52f461d0d9e22cdd471fb5c
Alfredo-Rojas <alfredot84@gmail.com>
2020-02-14 11:27:00 -0500

    While Loop added

--- JavaScript/Looping/main.js
+++ JavaScript/Looping/main.js
@@ -6,6 +6,22 @@ let john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
 //   console.log(john[i]);
 // }
 
+//Backwards
 // for (let i = john.length - 1; i >= 0; i--) {
 //   console.log(john[i]);
+// }
+
+//While
+
+// let i = 0;
+// while (i < john.length) {
+//   console.log(john[i]);
+//   i++;
+// }
+
+//Backwards
+// let i = john.length - 1;
+// while (i >= 0) {
+//   console.log(john[i]);
+//   i--;
 // }
