14d110f4d8890a67edde5b03d0c8cd352ed59f94
Alfredo-Rojas <alfredot84@gmail.com>
2020-02-15 09:48:08 -0500

    Continue and Break statements added

--- JavaScript/Looping/main.js
+++ JavaScript/Looping/main.js
@@ -1,6 +1,6 @@
 //For
 
-let john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+// let john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
 
 // for (let i = 0; i < john.length; i++) {
 //   console.log(john[i]);
@@ -24,4 +24,19 @@ let john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
 // while (i >= 0) {
 //   console.log(john[i]);
 //   i--;
+// }
+
+//Continue and Break Statements
+
+// let john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+//Continue
+// for (let i = 0; i < john.length; i++) {
+//   if (typeof john[i] !== 'string') continue;
+//   console.log(john[i]);
+// }
+
+//Break
+// for (let i = 0; i < john.length; i++) {
+//   if ( typeof john[i] !== 'string') break;
+//   console.log(john[i]);
 // }
